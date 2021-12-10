467ad6bc905e551d45536410b1076bce354a2f4d
Surkovo <54916117+Surkovo@users.noreply.github.com>
2021-12-10 16:38:16 -0600

    31:49

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,13 @@
 import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroConfig from './mikro-orm.config';
+//creating a main function since there is no top level await
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
 
-const orm = MikroORM.init();
+  const post = orm.em.create(Post, { title: 'first post' }); //this just creates an instance of post. it doesn't insert it into the DB
+  await orm.em.persistAndFlush(post); // this is what inserts to the DB
+};
 
-console.log('meow');
+main();
---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
+
+export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'), // path to the folder with migrations. Add the path from node to get the absolute path. join() joins the absolute path with whatever is the second arg
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files. added j to include js files
+  },
+  entities: [Post],
+  dbName: 'batut',
+  user: 'kylejohnson',
+  password: 'rooster20',
+  type: 'postgresql',
+  debug: __prod__,
+} as Parameters<typeof MikroORM.init>[0]; //need this for typescript to be happy see 25:30 in the video. so protip. look at what MikroORM.init accepts, then add that as a Parameter(this is a TS thing) to the config your exporting for exact types. Parameters returns an array so we want the first one to get all the types for MikroORM.init.
