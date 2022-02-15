ced95547ec785ecdfc73ebc4a057876944fbe64c
Surkovo <54916117+Surkovo@users.noreply.github.com>
2022-02-15 14:51:30 -0600

    2:16:00

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <>
+      <div>register page</div>
+    </>
+  );
+};
+
+export default Register; //you have to export default in next.js
